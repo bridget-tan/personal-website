0496e764aabb66ed07e534b93f273a0d10bedf1c
Bridget Tan <bridgett@andrew.cmu.edu>
2020-03-24 15:47:46 -0400

    Added JQuery to website
    Some links now open in new tabs.
    Added helper files.

---
+++ interactions.js
@@ -0,0 +1,11 @@
+$("#resume").click(function() {
+    window.open('documents/Bridget Tan Resume.pdf', '_blank');
+});
+
+$("#linkedin").click(function() {
+    window.open("https://www.linkedin.com/in/bridget-tan/", '_blank');
+});
+
+$("#github").click(function() {
+    window.open("https://github.com/bridget-tan", '_blank');
+});
