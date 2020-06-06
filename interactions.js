df13668bfb9761e8f006ddc5ca7fd0c4b1d8d6e3
Bridget Tan <bridgett@andrew.cmu.edu>
2020-06-05 23:05:32 -0400

    Cleaned up CSS and JQuery

--- interactions.js
+++ interactions.js
@@ -1,3 +1,6 @@
+/*Some JQuery adapted from: w3schools.com, cardadfar.com, 
+cardadfar.github.io/stuco_webdev_website, stackoverflow.com*/
+
 $("#resume").click(function() {
     window.open('documents/Bridget Tan Resume.pdf', '_blank');
 });
@@ -34,36 +37,6 @@ function openCity(evt, cityName) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-
 
 // https://stackoverflow.com/questions/41541559/multiple-slideshows-on-one-page-makes-the-first-one-not-work-anymore
 var sliderObjects = [];
@@ -96,7 +69,6 @@ function createSliderObjects() {
 }
 
 function showDivs(divObject, n) {
-  debugger;
   var i;
   if (n > divObject.slideContents.length) {
     divObject.slideIndex = 1
